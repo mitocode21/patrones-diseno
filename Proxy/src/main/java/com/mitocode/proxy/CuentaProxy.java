22cd9e10720df41895d7655fadd240f2c3f9e36e
juancamilo <juanccamachob94@gmail.com>
2018-10-19 23:49:26 -0500

    Reducción de validaciones en funciones

--- Proxy/src/main/java/com/mitocode/proxy/CuentaProxy.java
+++ Proxy/src/main/java/com/mitocode/proxy/CuentaProxy.java
@@ -18,34 +18,22 @@ public class CuentaProxy implements ICuenta {
 	@Override
 	public Cuenta retirarDinero(Cuenta cuenta, double monto) {
 		LOGGER.info("----Cuenta Proxy - Retirar Dinero----");
-		if (cuentaReal == null) {
-			cuentaReal = new CuentaBancoAImpl();
-			return cuentaReal.retirarDinero(cuenta, monto);
-		} else {
-			return cuentaReal.retirarDinero(cuenta, monto);
-		}
+		if (cuentaReal == null) cuentaReal = new CuentaBancoAImpl();
+		return cuentaReal.retirarDinero(cuenta, monto);
 	}
 
 	@Override
 	public Cuenta depositarDinero(Cuenta cuenta, double monto) {
 		LOGGER.info("----Cuenta Proxy - Depositar Dinero----");
-		if (cuentaReal == null) {
-			cuentaReal = new CuentaBancoAImpl();
-			return cuentaReal.depositarDinero(cuenta, monto);
-		} else {
-			return cuentaReal.depositarDinero(cuenta, monto);
-		}
+		if (cuentaReal == null) cuentaReal = new CuentaBancoAImpl();
+		return cuentaReal.depositarDinero(cuenta, monto);
 	}
 
 	@Override
 	public void mostrarSaldo(Cuenta cuenta) {
 		LOGGER.info("----Cuenta Proxy - Mostrar Dinero----");
-		if (cuentaReal == null) {
-			cuentaReal = new CuentaBancoAImpl();
-			cuentaReal.mostrarSaldo(cuenta);
-		} else {
-			cuentaReal.mostrarSaldo(cuenta);
-		}
+		if (cuentaReal == null) cuentaReal = new CuentaBancoAImpl();
+		cuentaReal.mostrarSaldo(cuenta);
 	}
 
 }
